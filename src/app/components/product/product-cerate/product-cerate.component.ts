9a4f7a70c0e78f3fd19e55a8f8fcba450f7f3cd5
loja <loja5@gmail.com>
2022-08-18 20:25:03 -0300

    componente criar produto

---
+++ src/app/components/product/product-cerate/product-cerate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-cerate',
+  templateUrl: './product-cerate.component.html',
+  styleUrls: ['./product-cerate.component.css']
+})
+export class ProductCerateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

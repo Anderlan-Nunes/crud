cf5a9f444da1a28c870dd9ee7fb11c7d455e3617
Anderlan pc1 <anderlan.dev@gmail.com>
2022-08-22 01:40:10 -0300

    outras maneiras de aceitar colocar null em um number como o price

--- src/app/components/product/product.model.ts
+++ src/app/components/product/product.model.ts
@@ -3,3 +3,15 @@ export interface Product {
     name: string
     price: number    
 }
+
+/**aceitar null no price
+ * export interface Product {
+    id?: number;
+    name: string;
+    price: number | null ;
+}
+
+ou colocar no arquito tsconfig.json    
+
+    "strictNullChecks": false
+ */

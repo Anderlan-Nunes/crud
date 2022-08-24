966388d4166443adb5647df61ebfa486a107c99c
loja <loja5@gmail.com>
2022-08-24 19:57:26 -0300

    componente atualizar produto. pegando o id da url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'
-import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 const routes: Routes = [

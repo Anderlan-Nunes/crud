be10692e171f4474d13c141ad651872ee6ffc740
loja <loja5@gmail.com>
2022-08-24 21:14:25 -0300

    componente atualizar produto- salvando as alteraçõe de cada produto

--- src/app/components/product/product-update/product-update.component.ts
+++ src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product!: Product
+
+  constructor(
+    private productServie: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productServie.readyById(id!).subscribe(product => {
+      this.product = product;
+    })
+  }
+
+  updateProduct(): void {
+    this.productServie.update(this.product).subscribe(() => {
+      this.productServie.showMessage('Produto atualizado com sucesso!')
+      this.router.navigate(["/products"]);
+    })
+  }
+
+  cancel() {
+    this.router.navigate(["/products"]);
   }
 
 }

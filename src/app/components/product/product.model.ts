14a98ea544f3128175a423e90bfeb653bb523dff
Anderlan pc1 <anderlan.dev@gmail.com>
2022-08-25 23:18:12 -0300

    resolvendo problema de não aceitar parametro null

--- src/app/components/product/product.model.ts
+++ src/app/components/product/product.model.ts
@@ -14,4 +14,16 @@ export interface Product {
 ou colocar no arquito tsconfig.json    
 
     "strictNullChecks": false
+
+
+Basta ir para tsconfig.json e definir "strictPropertyInitialization": false ou apenas ""strict": false
+
+Consegui resolver, estava com o mesmo problema, isso acontece porque a variavel ID está recebendo um valor nullo. Então, você só precisa acrescentar isso  if(id == null)   {  
+id = '" " ;  }  ,se o a variavel id for nulla, então ela recebe uma string vazia
+
+  product: Product = {
+    name: '',
+    price: null
+  };
+
  */

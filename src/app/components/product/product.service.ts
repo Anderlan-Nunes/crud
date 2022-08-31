7565e9085e4900701aa872b7bc9bd9c66fcddf96
Anderlan pc1 <anderlan.dev@gmail.com>
2022-08-31 19:20:00 -0300

    criando as mensagens de erro

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable, pipe } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,35 +15,60 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success'] //os dois valores vem do css, qndo false monstra sucess
     })
   }
 
   create(product: Product): Observable<Product>{
-    return this.http.post<Product>(this.baseAPIUrl, product)
+    return this.http.post<Product>(this.baseAPIUrl, product).pipe( // o pipe eh que caso aconteça um erro no meétodo post ele vai chamar o catchError
+      map(obj =>obj),
+      catchError((e) => this.errorHandler(e))
+    )
   }
 
   read(): Observable<Product[]> { // retorna uma lista de produto da api metodo eh consumido dentro product-read
-    return this.http.get<Product[]>(this.baseAPIUrl)
+    return this.http.get<Product[]>(this.baseAPIUrl).pipe(
+      map(obj =>obj),
+      catchError((e) => this.errorHandler(e))
+    )
 
   }
 //nao precisa ser number pq vem da url
   readyById(id: string): Observable<Product> {
     const url = `${this.baseAPIUrl}/${id}`;
-    return this.http.get<Product>(url)
+    return this.http.get<Product>(url).pipe(
+      map(obj =>obj),
+      catchError((e) => this.errorHandler(e))
+    )
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.baseAPIUrl}/${product.id}`;
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product).pipe(
+      map(obj =>obj),
+      catchError((e) => this.errorHandler(e))
+    )
   }
 
   delete(id: string) {
     const url = `${this.baseAPIUrl}/${id}`;
-    return this.http.delete<Product>(url)
+    return this.http.delete<Product>(url).pipe(
+      map(obj =>obj),
+      catchError((e) => this.errorHandler(e))
+    )
   }
+
+  errorHandler(e: any): Observable<any> {
+// podia ter criado esse codigo direto nos metodos mas por causa do tratamento do this foi melhor criar esse metodo
+ // uma forma de garantir q vai usar o this de forma certa foi criar um aronfunciton e levar esse método ai da a garantia q o this vai apontar pra instacia da classe atual 
+    
+    this.showMessage("Ocorreu um erro!", true) 
+    return EMPTY; // retorna um observable vazio observablou
+
+}
 }
